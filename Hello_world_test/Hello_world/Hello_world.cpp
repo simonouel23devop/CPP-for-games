ab6df8af41c3d3564ca60e03f7f3ece7f65e0ef3
Francois Ouellet <franouel14@hotmail.com>
2026-08-08 14:50:05 -0400

    Update Hello_world.cpp

--- Hello_world_test/Hello_world/Hello_world.cpp
+++ Hello_world_test/Hello_world/Hello_world.cpp
@@ -1,14 +1,13 @@
-// This is a simple C++ program   
+// Un simple programme en C++... 
+git commit - sauvegarde
 #include <iostream>
 #include <string>
 
 using namespace std; //On supprime les std...
 
-//Démarrage du programme principal..........==
-
-int main()
+int main()//Démarrage du programme principal...
 {
-    cout << "Hello World!\n";
+    cout << "Hello World!";
 
 
 return 0
