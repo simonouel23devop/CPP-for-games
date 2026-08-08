b59ae293f65ca78518a9b1ea764085cb475aec67
Francois Ouellet <franouel14@hotmail.com>
2026-08-07 21:06:15 -0400

    Correct syntax and enhance comments in Hello_world.cpp
    Fixed syntax errors and improved comments.

--- Hello_world_test/Hello_world/Hello_world.cpp
+++ Hello_world_test/Hello_world/Hello_world.cpp
@@ -1,13 +1,15 @@
+// This is a simple C++ program   
 #include <iostream>
 #include <string>
 
-using namespace std;
-//On supprime les std...
+using namespace std; //On supprime les std...
 
 //Démarrage du programme principal...
 int main()
 {
     cout << "Hello World!\n";
-}
 
-// This is a simple C++ program   
+
+return 0
+}
+ 
