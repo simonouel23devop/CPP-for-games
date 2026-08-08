27673973e9d2cbedfcd2be1cd5a4e4dc1d3f003e
Francois Ouellet <franouel14@hotmail.com>
2026-08-08 05:36:44 -0400

    Make JumpHeight a local variable in Update function
    Changed JumpHeight to a local variable within the Update function.

--- Hello_world_test/Build Process/Game.cpp
+++ Hello_world_test/Build Process/Game.cpp
@@ -1,12 +1,13 @@
 #include "Config.hpp"
 #include <iostream>
 #include "Game.hpp"
+#include <string>
 
 
 // function definition.
 void Update() {
 	
-	JumpHeight = 10.0f;
+	float JumpHeight = 10.0f; 
 
 	std::cout << "Jump height : " << JumpHeight << std::endl;
 	std::cout << "Jump duration : " << JumpDuration << std::endl;
