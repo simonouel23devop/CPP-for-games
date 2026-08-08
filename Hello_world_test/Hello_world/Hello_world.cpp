a3a4a7d741c005642f614410febd63976b284275
Francois Ouellet <franouel14@hotmail.com>
2026-08-07 21:02:03 -0400

    Refactor Hello World output to use 'cout'

--- Hello_world_test/Hello_world/Hello_world.cpp
+++ Hello_world_test/Hello_world/Hello_world.cpp
@@ -1,10 +1,13 @@
-
-
 #include <iostream>
+#include <string>
 
+using namespace std;
+//On supprime les std...
+
+//Démarrage du programme principal...
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
 
 // This is a simple C++ program   
