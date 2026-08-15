fcaa1107a3ce8543147d4ac1ad478d9c912ba02f
Simmus <simonouel12@gmail.com>
2026-08-15 19:39:12 -0400

    shared PTR and weak PTR pointer
    shared PTR and weak PTR pointer...

---
+++ Main_Folder/PointerPTRSmart/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+#include <iostream>
+#include <memory>
+
+class widget
+{
+public: 
+	explicit widget(int data) : data_(data) {} // explicit constructor to prevent implicit conversions
+
+	void do_something() // member function that does something with the widget
+	{
+		std::cout << "widget is doing something with value " << data_ << std::endl; 
+	}
+
+	int get_data() const { return data_; } // getter function to access the private data member
+
+private:
+	int data_;
+};
+
+int main()
+{
+	// use a shared_ptr to manage the widget
+	std::shared_ptr<widget> w = std::make_shared<widget>(42); // create a shared_ptr to a widget with data 42
+	w->do_something();
+
+	// access data via getter
+	std::cout << w->get_data() << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
