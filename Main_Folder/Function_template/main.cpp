615956cea81a3d164885513f1004671fb1d7b21c
Simmus <simonouel12@gmail.com>
2026-08-20 18:05:59 -0400

    function update
    function update ....

---
+++ Main_Folder/Function_template/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template <typename T>
+decltype(auto)	 max(const T* a, const T* b) {
+	if (!a || !b) return *a; // simple null check, return first if second is null
+	return (*a > *b) ? *a : *b;
+
+	void swap(T & a, T & b);
+
+		T tmp = *a;
+		a = b;
+
+
+		std::cout << "a = " << a << std::endl; // a
+		std::cout << "b = " << b << std::endl; // b
+}
+
+int main() {		
+	
+
+	int a = 5;
+	int b = 10;
+
+	int c = max(&a, &b);
+
+	std::cout << "c = " << c << std::endl; // print the results
+
+	return 0;
+}
